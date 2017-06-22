d7bebc0913bd9a50e167930d83873f82b390e515
鸽吱吱 <858134843@qq.com>
2017-06-22 15:00:11 +0800

    c

---
+++ views/site/create.php
@@ -0,0 +1,16 @@
+<?php
+
+use yii\helpers\Html;
+
+$this->title = '提交报修';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="repairs-create col-md-9">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <?= $this->render('_form', [
+        'model' => $model,  
+    ]) ?>
+
+</div>
